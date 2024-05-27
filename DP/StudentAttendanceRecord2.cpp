74527c0eaa5f9cf5d656123d1f3a74254a1278c7
Shruhrid Banthia <76119205+sbthycode@users.noreply.github.com>
2024-05-27 15:49:15 +0530

    Update and rename StudentAttendanceRecord2.md to StudentAttendanceRecord2.cpp

--- DP/StudentAttendanceRecord2.cpp
+++ DP/StudentAttendanceRecord2.cpp
@@ -1,19 +1,20 @@
-Problem link: https://leetcode.com/problems/student-attendance-record-ii/description/?envType=daily-question&envId=2024-05-26
+// Problem link: https://leetcode.com/problems/student-attendance-record-ii/description/?envType=daily-question&envId=2024-05-26
 
-An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
+// An attendance record for a student can be represented as a string where each character signifies whether the student was absent, late, or present on that day. The record only contains the following three characters:
 
-'A': Absent.
-'L': Late.
-'P': Present.
-Any student is eligible for an attendance award if they meet both of the following criteria:
+// 'A': Absent.
+// 'L': Late.
+// 'P': Present.
+// Any student is eligible for an attendance award if they meet both of the following criteria:
 
-The student was absent ('A') for strictly fewer than 2 days total.
-The student was never late ('L') for 3 or more consecutive days.
-Given an integer n, return the number of possible attendance records of length n that make a student eligible for an attendance award. The answer may be very large, so return it modulo 10^9 + 7.
+// The student was absent ('A') for strictly fewer than 2 days total.
+// The student was never late ('L') for 3 or more consecutive days.
+// Given an integer n, return the number of possible attendance records of length n that make a student eligible for an attendance award. The answer may be very large, so return it modulo 10^9 + 7.
 
-
-```
 // Approach: 3D DP in the number of days, count of absent so far and count of current consecutive leaves.
+#include <vector>
+
+using namespace std;
 class Solution {
 public:
     int mod = pow(10,9) + 7;
@@ -60,6 +61,4 @@ public:
         
     }
 };
-
-```
 
